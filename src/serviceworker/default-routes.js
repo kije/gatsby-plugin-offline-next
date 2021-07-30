e8cc2ad3b143be71a7ac080b224022a2fafde7de
Kim Jeker <git@kije.ch>
2021-07-30 16:33:32 +0200

    provide exports for individual default routes to allow for easier reuse of default route in consumer's custom sw implementation

--- src/serviceworker/default-routes.js
+++ src/serviceworker/default-routes.js
@@ -1,34 +1,50 @@
-import { registerRoute } from "workbox-routing"
-import { CacheFirst, StaleWhileRevalidate } from "workbox-strategies"
-import { CacheableResponsePlugin } from "workbox-cacheable-response"
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
-export function registerDefaultRoutes() {
+export function registerStaticBuildAssetsRoute () {
   registerRoute(
     /(\.js$|\.css$|static\/)/,
     new CacheFirst({
       plugins: [new CacheableResponsePlugin({ statuses: [200] })],
     }),
-    `GET`
-  )
+    `GET`,
+  );
+}
+
+export function registerPageDataRoute () {
   registerRoute(
     /^https?:.*\/page-data\/.*\.json/,
     new StaleWhileRevalidate({
       plugins: [new CacheableResponsePlugin({ statuses: [200] })],
     }),
-    `GET`
-  )
+    `GET`,
+  );
+}
+
+export function registerDefaultAssetsRoute () {
   registerRoute(
     /^https?:.*\.(png|jpg|jpeg|webp|avif|svg|gif|tiff|js|woff|woff2|json|css)$/,
     new StaleWhileRevalidate({
       plugins: [new CacheableResponsePlugin({ statuses: [200] })],
     }),
-    `GET`
-  )
+    `GET`,
+  );
+}
+
+export function registerGoogleFontsRoute () {
   registerRoute(
     /^https?:\/\/fonts\.googleapis\.com\/css/,
     new StaleWhileRevalidate({
       plugins: [new CacheableResponsePlugin({ statuses: [200] })],
     }),
-    `GET`
-  )
+    `GET`,
+  );
+}
+
+export function registerDefaultRoutes () {
+  registerStaticBuildAssetsRoute();
+  registerPageDataRoute();
+  registerDefaultAssetsRoute();
+  registerGoogleFontsRoute();
 }
